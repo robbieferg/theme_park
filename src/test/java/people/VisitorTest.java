defadfe2425032e40142b349f014de9dbd838436
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-02-07 17:31:04 +0000

    Visitor has list of visited attractions, can be added to.

--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,5 +1,6 @@
 package people;
 
+import attractions.Dodgems;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,10 +9,12 @@ import static org.junit.Assert.assertEquals;
 public class VisitorTest {
 
     Visitor visitor;
+    Dodgems dodgems;
 
     @Before
     public void before(){
         visitor = new Visitor(14, 1.2, 40.0);
+        dodgems = new Dodgems("Bumper Cars", 5);
     }
 
     @Test
@@ -27,5 +30,16 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void visitedAttractionsListStartsEmpty() {
+        assertEquals(0, visitor.getVisitedAttractions().size());
+    }
+
+    @Test
+    public void canAddVisitedAttraction() {
+        visitor.addVisitedAttraction(dodgems);
+        assertEquals(1, visitor.getVisitedAttractions().size());
     }
 }
