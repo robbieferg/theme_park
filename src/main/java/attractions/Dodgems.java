6c33ecf7862c2a0359ccccd07fed0c1724784136
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-02-07 16:35:03 +0000

    Dodgems implement ITicketed. MVP completed.

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,24 @@
 package attractions;
 
-public class Dodgems extends Attraction {
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class Dodgems extends Attraction implements ITicketed {
+    private double startingPrice;
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+        this.startingPrice = 4.50;
+    }
+
+    public double defaultPrice() {
+        return this.startingPrice;
+    }
+
+    public double priceFor(Visitor visitor) {
+        if (visitor.getAge() < 12) {
+            return defaultPrice() / 2;
+        }
+        return defaultPrice();
     }
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor1;
+    Visitor visitor2;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        visitor1 = new Visitor(9, 0.8, 12.73);
+        visitor2 = new Visitor(15, 0.8, 12.73);
+
     }
 
 
@@ -30,5 +36,18 @@ public class DodgemTest {
         assertEquals(0, dodgems.getVisitCount());
     }
 
+    @Test
+    public void hasDefaultPrice() {
+        assertEquals(4.50, dodgems.defaultPrice(), 0.0);
+    }
 
+    @Test
+    public void halfPriceIfVisitorUnder12() {
+        assertEquals(2.25, dodgems.priceFor(visitor1), 0.0);
+    }
+
+    @Test
+    public void normalPriceIfVisitorOver12() {
+        assertEquals(4.50, dodgems.priceFor(visitor2), 0.0);
+    }
 }
