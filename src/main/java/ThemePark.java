dab137df40852f012213c9c27a247c7c980c4eb9
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-02-07 17:16:41 +0000

    Theme Park has lists of attractions and stalls

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,41 @@
+import attractions.Attraction;
+import org.w3c.dom.Attr;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+
+    public ThemePark() {
+        this.attractions = new ArrayList<>();
+        this.stalls = new ArrayList<>();
+    }
+
+    public ArrayList<Attraction> getAttractions() {
+        return attractions;
+    }
+
+    public ArrayList<Stall> getStalls() {
+        return stalls;
+    }
+
+    public void addAttraction(Attraction attraction) {
+        attractions.add(attraction);
+    }
+
+    public void addStall(Stall stall) {
+        stalls.add(stall);
+    }
+
+    public void removeAttraction(Attraction attraction) {
+        attractions.remove(attraction);
+    }
+
+    public void removeStall(Stall stall) {
+        stalls.remove(stall);
+    }
+
+
 }
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,67 @@
+import attractions.Dodgems;
+import attractions.Park;
+import attractions.Playground;
+import attractions.RollerCoaster;
+import org.junit.Before;
+import org.junit.Test;
+import stalls.CandyflossStall;
+import stalls.IceCreamStall;
+import stalls.ParkingSpot;
+import stalls.TobaccoStall;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThemeParkTest {
+    ThemePark themePark;
+
+    Dodgems dodgems;
+    Park park;
+    Playground playground;
+    RollerCoaster rollerCoaster;
+
+    CandyflossStall candyflossStall;
+    IceCreamStall iceCreamStall;
+    TobaccoStall tobaccoStall;
+
+    @Before
+    public void before() {
+        dodgems = new Dodgems("Bumper Cars", 5);
+        park = new Park("Wonderland", 3);
+        playground = new Playground("Happy World", 4);
+        rollerCoaster = new RollerCoaster("The Death Machine", 1);
+        candyflossStall = new CandyflossStall("FlossLand", "Candy Floss Industries", ParkingSpot.A1, 2);
+        iceCreamStall = new IceCreamStall("Mr. Icee", "John Smith", ParkingSpot.B1, 5);
+        tobaccoStall = new TobaccoStall("Jimmy's Cigs", "FFred Jones", ParkingSpot.B3, 4);
+
+        themePark = new ThemePark();
+        themePark.addAttraction(dodgems);
+        themePark.addAttraction(park);
+        themePark.addAttraction(playground);
+        themePark.addAttraction(rollerCoaster);
+        themePark.addStall(candyflossStall);
+        themePark.addStall(iceCreamStall);
+        themePark.addStall(tobaccoStall);
+    }
+
+    @Test
+    public void canAddAttractions() {
+        assertEquals(4, themePark.getAttractions().size());
+    }
+
+    @Test
+    public void canAddStalls() {
+        assertEquals(3, themePark.getStalls().size());
+    };
+
+    @Test
+    public void canRemoveAttraction() {
+        themePark.removeAttraction(playground);
+        assertEquals(3, themePark.getAttractions().size());
+    }
+
+    @Test
+    public void canRemoveStall() {
+        themePark.removeStall(iceCreamStall);
+        assertEquals(2, themePark.getStalls().size());
+    }
+}
